603b3a9375217b4eae0293a4afe08758f8b9c749
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-15 13:06:00 -0500

    Setting up HtmlWebpackPlugin - Set up title to HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,6 +7,11 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
