28ad46afb9537d864785da7479ae7359bc8d6c5e
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-15 12:21:50 -0500

    Remove all prevoius lessons - To stay ready to the next one

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
-import Datajson from './cars.json';
 
 function component() {
   const element = document.createElement('div');
-
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-  console.log(Data);
-  console.log(Datajson);
-
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -12,28 +12,4 @@ module.exports = {
     compress: true,
     port: 9000,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader'],
-      },
-    ],
-  },
 };
