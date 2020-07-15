4bea63a077b4a39d014361db017917acd36751a4
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-15 11:35:03 -0500

    Add devServer config - Change port - Add path where js file is located

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
   module: {
     rules: [
@@ -19,6 +25,14 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader'],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader'],
       },
     ],
   },
